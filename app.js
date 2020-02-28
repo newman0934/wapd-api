ef0642c975d589b9be91da309aa18f2a39ce5e8d
java5575 <newman0934@gmail.com>
2020-02-29 01:03:29 +0800

    cors

--- app.js
+++ app.js
@@ -34,10 +34,7 @@ app.use('/api-docs', express.static('public'))
 app.use(
   cors({
     origin: [
-      'https://wap-d.com/',      
-      'http://localhost:8080',
-      'https://newman0934.github.io',
-      'https://easonlin0716.github.io'
+      'https://wap-d.com/'      
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
